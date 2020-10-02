93bbd60fa1a774041636db8d43bf22382cd8df37
Andrej Chudy <andrej@deepnote.com>
2020-10-02 11:46:28 +0200

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -3,15 +3,15 @@ set -euxo pipefail
 
 cd "$(dirname "$0")"
 # Configs
-cp ./user-utils/src/ipython/ipython_config.py $HOME/.ipython/profile_default/
-cp ./user-utils/src/jupyter/ipython_config.py $HOME/.jupyter/
+cp ./src/ipython/ipython_config.py $HOME/.ipython/profile_default/
+cp ./src/jupyter/ipython_config.py $HOME/.jupyter/
 
 # Code
 mkdir $HOME/.deepnote
-cp ./user-utils/src/jupyter/set_notebook_path.py $HOME/.deepnote
-cp ./user-utils/src/df/test.py $HOME/.deepnote
-cp ./user-utils/src/variable_explorer/variable_explorer.py $HOME/.deepnote
-cp ./user-utils/src/variable_explorer/variable_explorer_helpers.py $HOME/.deepnote
+cp ./src/jupyter/set_notebook_path.py $HOME/.deepnote
+cp ./src/df/test.py $HOME/.deepnote
+cp ./src/variable_explorer/variable_explorer.py $HOME/.deepnote
+cp ./src/variable_explorer/variable_explorer_helpers.py $HOME/.deepnote
 
 # Append to .bashrc
 echo 'cd ~/work \n \
